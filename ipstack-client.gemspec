e86744e723dc236768ed93a2b3f76a0bd623450a
Yuvraj Jaiswal <kumaryuvraj118@gmail.com>
2020-04-26 17:05:57 +0530

    Version 0.0.2 Added API Interface to IPStack APIs

--- ipstack-client.gemspec
+++ ipstack-client.gemspec
@@ -1,12 +1,15 @@
-Gem::Specification.new do |s|
-  s.name        = 'ipstack-client'
-  s.version     = '0.0.1'
-  s.date        = '2020-04-25'
-  s.summary     = "IPStack Client for Ruby"
-  s.description = "A simple Ruby Client Interface for accessing GEOIP services of IPStack"
-  s.authors     = ["Yuvraj Jaiswal"]
-  s.email       = 'kumaryuvraj118@gmail.com'
-  s.files       = ["lib/ipstack-client.rb"]
-  s.homepage    = 'https://github.com/yuvi17/ipstack-client'
-  s.license     = 'MIT'
+Gem::Specification.new do |spec|
+  spec.name        = 'ipstack-client'
+  spec.version     = '0.0.2'
+  spec.date        = '2020-04-25'
+  spec.summary     = "IPStack Client for Ruby"
+  spec.description = "A simple Ruby Client Interface for accessing GEOIP services of IPStack"
+  spec.authors     = ["Yuvraj Jaiswal"]
+  spec.email       = 'kumaryuvraj118@gmail.com'
+  spec.files       = ["lib/ipstack-client.rb","lib/api_interface.rb"]
+  spec.homepage    = 'https://github.com/yuvi17/ipstack-client'
+  spec.required_ruby_version = '>= 2.4.0'
+  spec.add_dependency 'redis', '>= 4.0.2'
+  spec.add_dependency 'rest-client', '>= 2.0.0'
+  spec.license     = 'MIT'
 end
--- lib/ipstack-client.rb
+++ lib/ipstack-client.rb
@@ -1,5 +1,82 @@
+# frozen_string_literal: true
+
+require 'redis'
+require 'json'
+require 'api_interface.rb'
+
 class IpstackClient
-  def self.find
-    puts "Hello"
+  def self.configure
+    yield self
+  end
+
+  def self.redis=(hash = {})
+    return unless @enable_caching
+
+    @redis = if hash.instance_of?(Redis)
+               hash
+             else
+               Redis.new(hash)
+             end
+  end
+
+  def self.redis
+    return unless @enable_caching
+    # use default redis if not set
+    @redis ||= Redis.new
+  end
+
+  def self.enable_caching=(enable_caching)
+    @enable_caching = enable_caching
+  end
+
+  def self.enable_caching
+    @enable_caching ||= true
+  end
+
+  def self.cache_period=(period)
+    @cache_period = period
+  end
+
+  # by default cache stuff for 6 hours
+  def self.cache_period
+    @cache_period ||= 6*60*60 if @enable_caching
+  end
+
+  def self.api_key=(key)
+    @api_key = key
+  end
+
+  def self.api_key
+    @api_key
+  end
+
+  def self.enable_https=(enable_https)
+    @enable_https = enable_https
+  end
+
+  def self.enable_https
+    @enable_https = true if @enable_https.nil?
+  end
+
+  class CountryLookup
+
+    def initialize
+      @api_key = IpstackClient.api_key
+      raise StandardError.new('API Key not specified') unless @api_key
+
+      @enable_caching = IpstackClient.enable_caching
+      if @enable_caching
+        @redis = IpstackClient.redis
+        @cache_period = IpstackClient.cache_period
+      end
+      @enable_https = IpstackClient.enable_https
+    end
+
+    def geoip_data ip
+      puts "Hello"
+      interface = ApiInterface.new(ip, @api_key, @enable_https)
+      interface.geoip_data
+    end
+
   end
 end
